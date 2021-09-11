e0dfcaf3bf512a1801a87c9cd57a9ea7a52a3952
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-11 16:41:47 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+const magic = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
 app.get('/greeting/:name', (req,res) => {
     res.send(`Whats up` + ` ` + req.params.name)
 })
@@ -17,6 +19,12 @@ app.get('/tip/:total/:tipPercentage', (req,res) => {
         + ` your tip is ${percentage}`)
 })
 
+
+
+app.get('/magic/:phrase', (req,res) => {
+    const itSpeaks = magic[Math.floor(Math.random() * magic.length)];
+    res.send('the magic conch says: ' + itSpeaks)
+})
 
 
 app.listen(3000, () => {
