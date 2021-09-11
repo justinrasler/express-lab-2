6c0efa42185c5b41b1312c6789acd59c5d23fafa
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-11 16:13:23 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 
 const app = express();
@@ -6,6 +8,14 @@ app.get('/greeting/:name', (req,res) => {
     res.send(`Whats up` + ` ` + req.params.name)
 })
 
+
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+    const percentage = req.params.tipPercentage / req.params.total * 100;
+    console.log(percentage)
+    console.log(req.params)
+    res.send(req.params.total + ' divided by ' + req.params.tipPercentage 
+        + ` your tip is ${percentage}`)
+})
 
 
 
